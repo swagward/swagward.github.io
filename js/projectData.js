09e79f8954fc542e28cbe44245b906095c55959e
Alex <alexpannell04@gmail.com>
2026-02-08 18:10:31 +0000

    Reworking projects page to take you to new page
    page is build using IDs and stored data to not have too many HTML files

---
+++ js/projectData.js
@@ -0,0 +1,25 @@
+export const projects =
+{
+    "swagaria":
+    {
+        title: "Swagaria",
+        subtitle: "Unity 2D Sandbox Game",
+        grade: "N/A (Personal Project)",
+        role: "Solo Developer",
+        description: "A Unity-based 2D sandbox game inspired by Terraria, focusing on cleanly designed architectures and optimisation.",
+        images: ["", "", ""],
+        website: "https://github.com/swagward/Swagaria-v4",
+        video: null
+    },
+    "camp-siege":
+        {
+            title: "Camp Siege",
+            subtitle: "Fast Paced Tower Defense",
+            grade: "B-",
+            role: "Solo Developer",
+            description: "A endless fast-paced tower defense game built in Unreal Engine 5, inspired Dungeon Defenders.",
+            images: ["", "", ""],
+            website: "https://github.com/swagward/Camp-Siege",
+            video: null
+        },
+}
